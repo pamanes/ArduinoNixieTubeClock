acb196904adf886e680c06af54e0b4147a8e0370
Alejandro Palacios <pamanes7@hotmail.com>
2015-01-15 14:53:40 -0800

    Update Cock_Sketch.ino

--- Cock_Sketch.ino
+++ Cock_Sketch.ino
@@ -8,7 +8,7 @@
 	
 	H   H : M   M : S   S  (Nixies)
 	|   |   |   |   |   |   (Rusian Driver has 10 output pins, each lighting up a number)
-    D   D   D   D   D   D   (Russian Driver K155NA1)
+        D   D   D   D   D   D   (Russian Driver K155NA1)
 	  \/      \/      \/    (4 pins to each Russian Driver)
 	  SF3     SF2     SF1   (each shift register has 8 output pins - 74HC595)
 	  
